1fe4f82fa4b39b214192491b91cfccf4286a2d81
emanuel <manuchoquepuma6@gmail.com>
2021-04-27 19:05:26 -0500

    Piraña

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using piraña.Models;
+
+namespace piraña.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Contacto> Contacto { get; set; }
+
+    }
+}
